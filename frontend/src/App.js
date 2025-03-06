e0acdee5087c98684c5f4a654fa96e65f66ae7d7
kerty <briikzdev@gmail.com>
2025-03-06 08:18:07 +0800

    routes fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './Pages/homepage';
+import AlumniDashboard from './Pages/Alumni/AlumniDashboard';
+import AlumniInformation from './Pages/Alumni/AlumniInformation';
+import StudentInformation from './Pages/Alumni/StudentInformation';
 
 function App() {
   return (
+   
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<AlumniDashboard />} />
+        <Route path="/AlumniInformation" element={<AlumniInformation />} />
+        <Route path="/StudentInformation" element={<StudentInformation />} />
       </Routes>
     </Router>
   );
